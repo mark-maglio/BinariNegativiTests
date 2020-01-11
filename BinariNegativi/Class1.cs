811370807e63fab9111923a3a512a4f18da7f9ab
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-11 12:18:49 +0100

    Add project files.

---
+++ BinariNegativi/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinariNegativi
+{
+    public class BinariNegativi
+    {
+        public static string VisualizzaMassimo(int a)
+        {
+            string risultato = Convert.ToString(a,2);
+            return risultato;
+        }
+    }
+}
